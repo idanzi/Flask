1488f21bc09cd95ff395e9ea8e2ad5fa5bec465a
wuxi <meidea@vip.qq.com>
2017-03-09 14:11:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ class Config(object):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:xixi20080608@localhost:3306/lishiwenhua"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost:3306/lishiwenhua"
 
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:xixi20080608@localhost:3306/lishiwenhua"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost:3306/lishiwenhua"
     # SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True  # SQLALCHEMY_TRACK_MODIFICATIONS 添加后警告不显示
